a102c83fb2efda131d9a0f5d79dbc4a755801eba
Chanipa-t <chanipa.t@clicknext.com>
2020-06-11 09:29:14 +0700

    Update ProfileScreen.js

--- tabTemplate/Page/ProfileScreen.js
+++ tabTemplate/Page/ProfileScreen.js
@@ -5,7 +5,7 @@ import { Button, Text, View } from 'react-native';
 function ProfileScreen({ navigation }) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Profile screen </Text>
+            <Text>Profile screen</Text>
             
         </View>
     );
